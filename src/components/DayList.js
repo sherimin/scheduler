3a6fdb53fc56a0c655c504eb57020e5972f3f153
Sheri Lu <sherisherilu@gmail.com>
2023-01-25 11:51:09 -0800

    Update DayList with child key. Add DayList to nav bar.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,6 +4,7 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
     const eachDay = props.days.map((day) => {
         return <DayListItem
+            key={day.name}
             name={day.name}
             spots={day.spots}
             selected={day.name === props.day}
