0594c58f8b9136d6e0e7da4449351a96ea0afe7e
Sheri Lu <sherisherilu@gmail.com>
2023-01-26 14:03:07 -0800

    Add InterviewerList component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -11,13 +11,13 @@ export default function InterviewerListItem(props) {
     });
 
     return (
-        <li className={interviewerClass} onClick={() => {props.setInterviewer(props.name) } }>
+        <li className={interviewerClass} onClick={() => {props.setInterviewer(props.id) } }>
         <img
           className={interviewerImgClass}
           src={props.avatar}
           alt={props.name}
         />
-        {props.name}
+        {props.selected && props.name}
         </li>
     )
 }
