2532ccfcdfc0107cbe495070b603d17c74815735
Sheri Lu <sherisherilu@gmail.com>
2023-01-27 14:34:18 -0800

    Add components for Appointment

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import "components/Appointment/styles.scss";
+import React from "react";
+
+export default function Appointment(props) {
+    return (
+        <article className="appointment"></article>
+    );
+}
