5e42454a4ae6901ffe86b52725a18658fd6d7774
Sheri Lu <sherisherilu@gmail.com>
2023-02-01 19:11:11 -0800

    Fix problems with DayList

--- src/components/Application.js
+++ src/components/Application.js
@@ -50,17 +50,16 @@ const appointments = {
 
 export default function Application(props) {
 
-
-    useEffect(() => {
-      const URL = "http://localhost:8001/api/days";
-      axios.get(URL).then(response => {
-        setDay(response.data)
-      })
-    }, [])
-
-
   const [day, setDay] = useState("Monday");
   const [days, setDays] = useState([]);
+
+    useEffect(() => {
+      const URL = 'http://localhost:8001/api/days';
+
+      axios.get(URL).then(response => {
+        setDays(response.data)
+      })
+    }, [])
 
   return (
     <main className="layout">
