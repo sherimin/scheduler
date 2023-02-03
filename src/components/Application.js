221dcde951ef49b87b180ef28d5e733465e08055
Sheri Lu <sherisherilu@gmail.com>
2023-02-03 11:16:46 -0800

    Refactor Application with setState

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
+//import { getAppointmentsForDay } from "helpers/selectors";
 
 //mock data
 const appointments = {
@@ -50,16 +51,23 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
 
-    useEffect(() => {
-      const URL = 'http://localhost:8001/api/days';
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {}
+  });
 
-      axios.get(URL).then(response => {
-        setDays(response.data)
-      })
-    }, [])
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+
+  useEffect(() => {
+    const URL = 'http://localhost:8001/api/days';
+
+    axios
+      .get(URL)
+      .then(response => setDays(response.data));
+    })
 
   return (
     <main className="layout">
@@ -72,8 +80,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        day={day}
+        days={state.days}
+        day={state.day}
         setDay={setDay}
       />
       </nav>
