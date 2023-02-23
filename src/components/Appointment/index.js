3150e938d769529ffaceba300ec82d35a6976462
Sheri Lu <sherisherilu@gmail.com>
2023-02-23 15:26:53 -0800

    Add cypress testing for booking interviews

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -103,13 +103,13 @@ export default function Appointment(props) {
             {mode === ERROR_DELETE && (
                 <Error
                   message="Could not cancel appointment"
-                  onClose={() => back}
+                  onClose={back}
                 />
             )}
             {mode === ERROR_SAVE && (
                 <Error
                   message="Could not save appointment"
-                  onClose={() => back}
+                  onClose={back}
                 />
             )}
         </article>
