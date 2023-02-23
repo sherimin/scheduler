06e44bca35d23a6809c1620dabd020142fbf6469
Sheri Lu <sherisherilu@gmail.com>
2023-02-23 15:48:39 -0800

    Add testing for editing interviews

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -23,4 +23,22 @@ describe("Appointments", () => {
       cy.contains(".appointment__card--show", "Sylvia Palmer");
 
     });
+
+    it("should edit an interview", () => {
+      cy.get("[alt=Edit]")
+        .first()
+        .click( { force: true} );
+
+      cy.get("[data-testid=student-name-input]")
+        .clear()
+        .type("Lydia Miller-Jones")
+
+      cy.get("[alt='Tori Malcolm']").click();
+
+      cy.contains("Save")
+        .click();
+
+      cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+      cy.contains(".appointment__card--show", "Tori Malcolm");
+    });
   });
