59c0ee94e08c974eb1ce791a14a9f2c34b17e7e9
Sheri Lu <sherisherilu@gmail.com>
2023-02-23 16:46:52 -0800

    Add Cypress testing for deleting interviews

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -41,4 +41,18 @@ describe("Appointments", () => {
       cy.contains(".appointment__card--show", "Lydia Miller-Jones");
       cy.contains(".appointment__card--show", "Tori Malcolm");
     });
+
+    it("should cancel an interview", () => {
+        cy.get("[alt=Delete]")
+          .click({ force: true });
+  
+        cy.contains("Confirm").click();
+  
+        cy.contains("Deleting").should("exist");
+
+        cy.contains("Deleting").should("not.exist");
+
+        cy.contains(".appointment__card--show", "Archie Cohen")
+          .should("not.exist");
+      });
   });
